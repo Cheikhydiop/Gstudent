b1185cb0ec0474278126bb525ca69865096ef731
diop <1000dioop@gmail.com>
2024-10-05 05:23:31 +0200

    first commit

--- src/main/java/com/wane/data/repository/ParentRepository.java
+++ src/main/java/com/wane/data/repository/ParentRepository.java
@@ -1,11 +1,12 @@
 package com.wane.data.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @NoRepositoryBean
-public interface  ParentRepository <T, ID> extends JpaRepository<T, ID> {
+public interface ParentRepository<T, ID> extends JpaRepository<T, ID> {
+
 
 }
